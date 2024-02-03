771f7b6fcedea59bf7958b672de8e6aa888805a8
David Yeh <davidmilk880330@gmail.com>
2024-02-03 21:00:27 +0800

    add filtering

--- read.py
+++ read.py
@@ -14,3 +14,11 @@ for d in data:
 	sum_len += len (d)
 	average_len = sum_len / len(data)
 print('留言的平均長度是', average_len)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
